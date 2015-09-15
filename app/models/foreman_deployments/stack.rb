e678d03ad70f5cab02024a97b677aca44138f6a2
Tomas Strachota <tstrachota@redhat.com>
2015-09-15 12:48:09 +0200

    Fixes #11815 - update stack via API

--- app/models/foreman_deployments/stack.rb
+++ app/models/foreman_deployments/stack.rb
@@ -1,5 +1,7 @@
 module ForemanDeployments
   class Stack < ActiveRecord::Base
+    has_many :configurations, :class_name => 'ForemanDeployments::Configuration'
+
     validates :name, :presence => true, :uniqueness => true
     validates :definition, :presence => true
 
