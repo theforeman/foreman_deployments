43578858767d1f112efd6b0caeea8adb293056fc
Tomas Strachota <tstrachota@redhat.com>
2015-09-21 10:27:56 +0200

    Refs #11882 - taxonomy handling in a concern

---
+++ app/models/foreman_deployments/concerns/belongs_to_single_taxonomy.rb
@@ -0,0 +1,42 @@
+module ForemanDeployments
+  module Concerns
+    module BelongsToSingleTaxonomy
+      extend ActiveSupport::Concern
+
+      included do
+        validates :organization_id, :presence => true, :if => lambda { SETTINGS[:organizations_enabled] }
+        validates :location_id, :presence => true, :if => lambda { SETTINGS[:locations_enabled] }
+
+        belongs_to :location
+        belongs_to :organization
+
+        if SETTINGS[:locations_enabled]
+          scoped_search :in => :location, :on => :title, :rename => :location, :complete_value => true
+          scoped_search :on => :location_id, :complete_enabled => false, :only_explicit => true
+        end
+        if SETTINGS[:organizations_enabled]
+          scoped_search :in => :organization, :on => :title, :rename => :organization, :complete_value => true
+          scoped_search :on => :organization_id, :complete_enabled => false, :only_explicit => true
+        end
+
+        scope :no_location,     lambda { where(:location_id => nil) }
+        scope :no_organization, lambda { where(:organization_id => nil) }
+
+        default_scope do
+          where(taxonomy_conditions)
+        end
+      end
+
+      module ClassMethods
+        def taxonomy_conditions
+          org = Organization.expand(Organization.current) if SETTINGS[:organizations_enabled]
+          loc = Location.expand(Location.current) if SETTINGS[:locations_enabled]
+          conditions = {}
+          conditions[:organization_id] = Array(org).map(&:subtree_ids).flatten.uniq if org.present?
+          conditions[:location_id] = Array(loc).map(&:subtree_ids).flatten.uniq if loc.present?
+          conditions
+        end
+      end
+    end
+  end
+end
--- app/models/foreman_deployments/deployment.rb
+++ app/models/foreman_deployments/deployment.rb
@@ -1,55 +1,15 @@
 module ForemanDeployments
   class Deployment < ActiveRecord::Base
     include Authorizable
+    include ForemanDeployments::Concerns::BelongsToStackTaxonomy
 
     belongs_to :configuration, :class_name => 'ForemanDeployments::Configuration', :autosave => true
 
     validates :name, :presence => true
     validates :configuration,   :presence => true
-    validates :organization_id, :presence => true, :if => lambda { SETTINGS[:organizations_enabled] }
-    validates :organization_id,
-              :inclusion => {
-                :in => lambda { |d| d.stack.organizations.map(&:id) },
-                :message => _("Deployment's organization must be one of the stack's organizations.")
-              },
-              :if => lambda { |d| SETTINGS[:organizations_enabled] && !d.stack.nil? }
-    validates :location_id, :presence => true, :if => lambda { SETTINGS[:locations_enabled] }
-    validates :location_id,
-              :inclusion => {
-                :in => lambda { |d| d.stack.locations.map(&:id) },
-                :message => _("Deployment's location must be one of the stack's locations.")
-              },
-              :if => lambda { |d| SETTINGS[:organizations_enabled] && !d.stack.nil? }
-
-    belongs_to :location
-    belongs_to :organization
 
     scoped_search :on => :id, :complete_value => false
     scoped_search :on => :name, :complete_value => true, :default_order => true
-    if SETTINGS[:locations_enabled]
-      scoped_search :in => :location, :on => :title, :rename => :location, :complete_value => true
-      scoped_search :on => :location_id, :complete_enabled => false, :only_explicit => true
-    end
-    if SETTINGS[:organizations_enabled]
-      scoped_search :in => :organization, :on => :title, :rename => :organization, :complete_value => true
-      scoped_search :on => :organization_id, :complete_enabled => false, :only_explicit => true
-    end
-
-    scope :no_location,     lambda { where(:location_id => nil) }
-    scope :no_organization, lambda { where(:organization_id => nil) }
-
-    default_scope do
-      where(taxonomy_conditions)
-    end
-
-    def self.taxonomy_conditions
-      org = Organization.expand(Organization.current) if SETTINGS[:organizations_enabled]
-      loc = Location.expand(Location.current) if SETTINGS[:locations_enabled]
-      conditions = {}
-      conditions[:organization_id] = Array(org).map(&:subtree_ids).flatten.uniq if org.present?
-      conditions[:location_id] = Array(loc).map(&:subtree_ids).flatten.uniq if loc.present?
-      conditions
-    end
 
     def stack
       configuration.stack if configuration
