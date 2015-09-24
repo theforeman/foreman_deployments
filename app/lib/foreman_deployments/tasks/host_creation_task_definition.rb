ab5b8dc3effb63054cdf79e950e69488d569d428
Tomas Strachota <tstrachota@redhat.com>
2015-09-24 16:15:22 +0200

    Refs #10462 - complexity refactor

--- app/lib/foreman_deployments/tasks/host_creation_task_definition.rb
+++ app/lib/foreman_deployments/tasks/host_creation_task_definition.rb
@@ -23,19 +23,21 @@ module ForemanDeployments
         object = object_type.new(initial_params)
         object = set_parameters(object, object_params)
 
-        if object.is_a? Host::Managed
-          object_params['build'] ||= true
-          # Set compute attributes from a compute profile
-          if object.compute_resource_id && object.compute_profile_id
-            profile_attributes = object.compute_resource.compute_profile_attributes_for(object.compute_profile_id)
-            object.compute_attributes = profile_attributes.merge(object.compute_attributes || {})
-          end
-          # Merge interfaces from a compute profile
-          merge = InterfaceMerge.new
-          merge.run(object.interfaces, object.compute_resource.try(:compute_profile_for, object.compute_profile_id))
-        end
+        initialize_manged_host(object, object_params) if object.is_a? Host::Managed
 
         object
+      end
+
+      def self.initialize_manged_host(object, object_params)
+        object_params['build'] ||= true
+        # Set compute attributes from a compute profile
+        if object.compute_resource_id && object.compute_profile_id
+          profile_attributes = object.compute_resource.compute_profile_attributes_for(object.compute_profile_id)
+          object.compute_attributes = profile_attributes.merge(object.compute_attributes || {})
+        end
+        # Merge interfaces from a compute profile
+        merge = InterfaceMerge.new
+        merge.run(object.interfaces, object.compute_resource.try(:compute_profile_for, object.compute_profile_id))
       end
     end
   end
