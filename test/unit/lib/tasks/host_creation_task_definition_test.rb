0e5ade01f6f2b185974c3f869929cb08723c6681
Tomas Strachota <tstrachota@redhat.com>
2015-10-20 09:17:20 +0000

    Fixes #12170 - apply attributes from hostgroups on hosts

--- test/unit/lib/tasks/host_creation_task_definition_test.rb
+++ test/unit/lib/tasks/host_creation_task_definition_test.rb
@@ -7,4 +7,20 @@ class HostCreationTaskDefinitionTest < ActiveSupport::TestCase
     action = definition_class.build('class' => 'Host').dynflow_action
     assert_equal(ForemanDeployments::Tasks::HostCreationTaskDefinition::Action, action)
   end
+
+  describe 'object creation' do
+    test 'it applies parameters from a hostgroup' do
+      hostgroup = FactoryGirl.create(:hostgroup, :with_puppetclass, :with_puppet_orchestration)
+
+      object = definition_class.create_object(
+        'class' => 'Host::Managed',
+        'params' => {
+          'hostgroup_id' => hostgroup.id
+        }
+      )
+      assert_equal(hostgroup.environment_id, object.environment_id)
+      assert_equal(hostgroup.puppet_proxy_id, object.puppet_proxy_id)
+      assert_equal(hostgroup.puppet_ca_proxy_id, object.puppet_ca_proxy_id)
+    end
+  end
 end
